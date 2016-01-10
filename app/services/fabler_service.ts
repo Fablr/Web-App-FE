2fcd221764bd4961376f4842a795b0e525928527
Dustin Replogle <dustin.replogle@fabler.fm>
2016-01-09 20:18:30 -0600

    Updated to check to make sure user is logged in before refreshing token

--- app/services/fabler_service.ts
+++ app/services/fabler_service.ts
@@ -38,28 +38,30 @@ export class FablerService {
 		return localStorage.getItem('loggedIn');
 	}
 	public get_token() {
-		if(new Date < new Date(localStorage.getItem('token_expiration'))) {
-			return localStorage.getItem('access_token');
-		}
-		if(new Date > new Date(localStorage.getItem('token_expiration'))) {
-			var headers = new Headers();
-			headers.append('Content-Type', 'application/x-www-form-urlencoded');
-			this.http.post(this.api_http + 'o/token/', 'grant_type=refresh_token' + '&client_id=' +
-				this.client + '&refresh_token=' + localStorage.getItem('refresh_token'), {
-
-				headers: headers
-			})
-			.subscribe(
-				data => {
-					localStorage.setItem('token_expiration', String(new Date(Number(new Date()) - Number(-(new Date(data.json().expires_in * 1000))))));
-					localStorage.setItem('access_token', data.json().access_token);
-					localStorage.setItem('refresh_token', data.json().refresh_token);
-					localStorage.setItem('expires_in', data.json().expires_in);
-				},
-				err => console.log(err),
-				() => console.log('Complete')
-			);
-			return localStorage.getItem('access_token');
+		if(this.isAuth()) {
+			if(new Date < new Date(localStorage.getItem('token_expiration'))) {
+				return localStorage.getItem('access_token');
+			}
+			if(new Date > new Date(localStorage.getItem('token_expiration'))) {
+				var headers = new Headers();
+				headers.append('Content-Type', 'application/x-www-form-urlencoded');
+				this.http.post(this.api_http + 'o/token/', 'grant_type=refresh_token' + '&client_id=' +
+					this.client + '&refresh_token=' + localStorage.getItem('refresh_token'), {
+	
+					headers: headers
+				})
+				.subscribe(
+					data => {
+						localStorage.setItem('token_expiration', String(new Date(Number(new Date()) - Number(-(new Date(data.json().expires_in * 1000))))));
+						localStorage.setItem('access_token', data.json().access_token);
+						localStorage.setItem('refresh_token', data.json().refresh_token);
+						localStorage.setItem('expires_in', data.json().expires_in);
+					},
+					err => console.log(err),
+					() => console.log('Complete')
+				);
+				return localStorage.getItem('access_token');
+			}
 		}
 	}
 	public get_username() {
